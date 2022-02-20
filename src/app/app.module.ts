be0a7055536d0246e14227ee7665f868b10e3f38
Omoefe Ugboma <Omoefe.Ugboma@investment-one.com>
2022-02-20 22:35:08 +0100

    Installed configured the ngrx/effects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,10 @@ import { HeaderComponent } from './shared/components/header/header.component';
 import { AppRoutingModule } from './app-routing.module';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
-import { appReducer } from './store/app.state';
+// import { appReducer } from './store/app.state';
 import { ReactiveFormsModule } from '@angular/forms';
+import { EffectsModule } from '@ngrx/effects';
+
 
 @NgModule({
   declarations: [
@@ -19,12 +21,17 @@ import { ReactiveFormsModule } from '@angular/forms';
 
   ],
   imports: [
-    BrowserModule,
+  
+  BrowserModule,
     ReactiveFormsModule,
+    EffectsModule.forRoot([]),
     StoreModule.forRoot({}),
     AppRoutingModule,
     StoreDevtoolsModule.instrument({ 
-      maxAge: 25, logOnly: environment.production })
+      maxAge: 25, logOnly: environment.production }),
+     
+    
+      
   ],
   providers: [],
   bootstrap: [AppComponent]
